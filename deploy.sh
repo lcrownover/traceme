43e3753eebfa673c7f15b4d6faac092f3f4b75fe
Lucas Crownover <lcrownover127@gmail.com>
2023-08-14 21:10:09 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 docker build -t lcrowncrpublic.azurecr.io/traceme .
 docker push lcrowncrpublic.azurecr.io/traceme
-az container create --resource-group rg-prometheus-mock --name traceme --image lcrowncrpublic.azurecr.io/traceme --port 3333
+az container create --resource-group rg-prometheus-mock --name traceme --image lcrowncrpublic.azurecr.io/traceme --ports 3333 --dns-name-label traceme
